c76d1904868538513f15bd088e1c86f9d25f842a
Loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2026-02-25 10:01:55 +0100

    yapp

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 import express from "express"
+import pool from "../config/db.js"
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.render("index.njk",
-        { title: "Node js, mannen", message: "Tjabba tjena hallå!" }
-    )
-})
-
-router.get('/error', (req, res) => {
-    throw new Error('Test error')
+router.get("/", async (req, res, next) => {
+    try {
+        const [rows] = await pool.query(`
+            SELECT post.id, post.content, post.created_at, user.name
+            FROM post
+            JOIN user ON post.user_id = user.id
+            ORDER BY post.created_at DESC
+        `)
+        res.render("index.njk", { title: "Mikroblogg", posts: rows })
+    } catch (err) {
+        next(err)
+    }
 })
 
 export default router
